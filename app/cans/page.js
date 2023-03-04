87376cbb2b7624e74b1f81755f4a126fc48a6239
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-04 16:08:39 +0100

    touched up the fonts and code structure

--- app/cans/page.js
+++ app/cans/page.js
@@ -1,18 +1,11 @@
-import { Inter } from 'next/font/google'
 // import styles from './page.module.css'
 // import Link from "next/link"
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ['latin']
-})
 
 export default function Home() {
   return (
-    <main className={inter.className}>
       <div>
         <h1>Cans</h1>
       </div>
-    </main>
   )
 }
--- app/largeflasks/page.js
+++ app/largeflasks/page.js
@@ -1,20 +1,14 @@
-import { Inter } from 'next/font/google'
 // import styles from './page.module.css'
 // import Link from "next/link"
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ['latin']
-})
+
 
 export default function Home() {
   return (
-    <main className={inter.className}>
       <div>
         <h1>
           Large flasks
         </h1>
       </div>
-    </main>
   )
 }
